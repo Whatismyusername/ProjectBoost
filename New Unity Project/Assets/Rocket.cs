4f09e13237e8c0fa2995cb2d1316e1cf30aa2248
Whatismyusername <linhong.zhou@mihsnyc.org>
2020-01-09 16:52:38 -0500

    Created Rocket Ship + basic controlls

---
+++ New Unity Project/Assets/Rocket.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidBody;
+    private Vector3 ThrustForce = new Vector3(0, 1500, 0);
+    private Vector3 Rotation = new Vector3(0, 0, 100);
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        OnUserInput();
+    }
+
+    private void OnUserInput() {
+        if (Input.GetKey(KeyCode.Space)) {
+            //Thrust
+            rigidBody.AddRelativeForce(ThrustForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+            //Turn left
+            transform.Rotate(Rotation * Time.deltaTime);
+
+        } else if (Input.GetKey(KeyCode.D)) {
+            //Turn Right
+            transform.Rotate(-Rotation * Time.deltaTime);
+        }
+        
+    }
+}
