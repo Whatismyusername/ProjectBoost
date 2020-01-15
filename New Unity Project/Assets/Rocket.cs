80065970df4779cc3bd00fa6d276c8c7e24e9bac
Whatismyusername <linhong.zhou@mihsnyc.org>
2020-01-14 20:46:27 -0500

    Added transition between levels and audio

--- New Unity Project/Assets/Rocket.cs
+++ New Unity Project/Assets/Rocket.cs
@@ -1,36 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Rocket : MonoBehaviour
 {
 
     Rigidbody rigidBody;
     AudioSource audioSource;
+    [SerializeField] AudioClip ThrustingSound;
+    [SerializeField] AudioClip WinningSound;
+    [SerializeField] AudioClip LosingSound;
 
     Vector3 ThrustForce = new Vector3(0, 1200f, 0);
     Vector3 Rotation = new Vector3(0, 0, 180f);
+
+    enum Status { Won, Lost, InProgress };
+    Status gameStatus;
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = GetComponent<Rigidbody>();
         audioSource = GetComponent<AudioSource>();
+        gameStatus = Status.InProgress;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Thrust();
-        Rotate();
+        if(gameStatus == Status.InProgress) {
+            Thrust();
+            Rotate();
+        }
+        
     }
-
     private void Thrust() {
         if (Input.GetKey(KeyCode.Space)) {
             //Thrust
             rigidBody.AddRelativeForce(ThrustForce * Time.deltaTime);
         } 
         if (Input.GetKeyDown(KeyCode.Space)) {
-            audioSource.Play();
+            audioSource.PlayOneShot(ThrustingSound);
         }
         else if(Input.GetKeyUp(KeyCode.Space)) {
             audioSource.Stop();
@@ -53,6 +61,7 @@ public class Rocket : MonoBehaviour
 
     private void OnCollisionEnter(Collision other) {
         
+        if ( gameStatus != Status.InProgress) {return;}
         switch(other.gameObject.tag) {
             case "Friendly":
                 break;
@@ -62,15 +71,37 @@ public class Rocket : MonoBehaviour
                 // Freezes motion
                 rigidBody.velocity = new Vector3(0, 0, 0);
                 break;
+            case "Finish":
+                BeginToWin();
+                gameStatus = Status.Won;
+                break;
             default:
-                Debug.Log("DEAD");
+                BeginToLose();
+                gameStatus = Status.Lost;
                 break;
         }
     }
 
-    private void fixRotation() {
-        if (transform.position.z != 0) {
-            transform.position = new Vector3 (transform.position.x, transform.position.y, 0f);
-        }
+    private void BeginToWin() {
+        
+        audioSource.Stop();
+        audioSource.PlayOneShot(WinningSound);
+        Invoke("LoadNextLevel", 1f);
+
+    }
+
+    private void BeginToLose() {
+        audioSource.Stop();
+        audioSource.PlayOneShot(LosingSound);
+        Invoke("LoadFirstLevel", 1f);
+
+    }
+
+    private void LoadNextLevel() {
+        SceneManager.LoadScene("Level02");
+    }
+
+    private void LoadFirstLevel() {
+        SceneManager.LoadScene("Level01");
     }
 }
